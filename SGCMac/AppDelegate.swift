c04572e140dc8fc337756ad8315f7a2791bef2f5
장준우 <timingsniper@jangjun-uui-macbook-pro.local>
2016-10-22 20:45:54 +0800

    Initial Commit

---
+++ SGCMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SGCMac
+//
+//  Created by 장준우 on 2016. 10. 22..
+//  Copyright © 2016년 Joonwoo Percy Jang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
